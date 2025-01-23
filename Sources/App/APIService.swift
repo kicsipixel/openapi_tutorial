50595ff70fd2974902db03c97334262d622e7818
Szabolcs Toth <tsz@purzelbaum.hu>
2025-01-23 12:50:24 +0100

    Show was implemented.

--- Sources/App/APIService.swift
+++ Sources/App/APIService.swift
@@ -14,12 +14,14 @@ struct APIServiceImpl: APIProtocol {
   }
 
   // MARK: - Server health
-  func healthCheck(_: APIService.Operations.healthCheck.Input) async throws -> APIService.Operations.healthCheck.Output {
+  func healthCheck(_: APIService.Operations.healthCheck.Input) async throws
+    -> APIService.Operations.healthCheck.Output {
     .ok(.init())
   }
 
   // MARK: - Create
-  func createPerson(_ input: APIService.Operations.createPerson.Input) async throws -> APIService.Operations.createPerson.Output {
+  func createPerson(_ input: APIService.Operations.createPerson.Input) async throws
+    -> APIService.Operations.createPerson.Output {
     let id = UUID()
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "yyyy-MM-dd"
@@ -55,6 +57,67 @@ struct APIServiceImpl: APIProtocol {
       )
       """
     )
+
     return .created(.init())
+  }
+
+  // MARK: - List
+  func listPeople(_: APIService.Operations.listPeople.Input) async throws
+    -> APIService.Operations.listPeople.Output {
+    var people = [Components.Schemas.Person]()
+
+    let stream = try await client.query(
+      """
+      SELECT
+        id,
+        number_of_publications,
+        first_name,
+        last_name,
+        date_of_birth,
+        is_active
+      FROM
+        openapi_tutorial
+      """
+    )
+
+      for try await (id, number_of_publications, first_name, last_name, date_of_birth, is_active) in stream.decode((String, Int, String, String, Date, Bool).self) {
+      people.append(
+        .init(
+          id: id,
+          number_of_publications: number_of_publications,
+          name: .init(first_name: first_name, last_name: last_name),
+          date_of_birth: "\(date_of_birth)",
+          is_active: is_active))
+    }
+
+    return .ok(.init(body: .json(people)))
+  }
+  
+  // MARK: - Show
+  func getPersonById(_ input: APIService.Operations.getPersonById.Input) async throws -> APIService.Operations.getPersonById.Output {
+    let id = input.path.id
+    
+    print(id)
+    
+    let stream = try await client.query(
+      """
+      SELECT
+        id,
+        number_of_publications,
+        first_name,
+        last_name,
+        date_of_birth,
+        is_active
+      FROM
+        openapi_tutorial
+      WHERE id = id
+      """
+    )
+    
+    for try await (id, number_of_publications, first_name, last_name, date_of_birth, is_active) in stream.decode((String, Int, String, String, Date, Bool).self) {
+      return .ok(.init(body: .json(Components.Schemas.Person(id: id, number_of_publications: number_of_publications, name: .init(first_name: first_name, last_name: last_name), date_of_birth: "\(date_of_birth)", is_active: is_active))))
+    }
+    
+    return .notFound(.init())
   }
 }
--- Sources/App/App.swift
+++ Sources/App/App.swift
@@ -10,17 +10,17 @@ import PostgresNIO
 struct AppCommand: AsyncParsableCommand {
   @Option(name: .shortAndLong)
   var hostname: String = "127.0.0.1"
-  
+
   @Option(name: .shortAndLong)
   var port: Int = 8080
-  
+
   func run() async throws {
     let router = Router()
     router.middlewares.add(LogRequestsMiddleware(.info))
-    
+
     var logger = Logger(label: "openapi_tutorial")
     logger.logLevel = .debug
-    
+
     let env = try await Environment.dotEnv()
       // env.get("DATABASE_HOST") ??
     let client = PostgresClient(configuration: .init(host:  "localhost",
@@ -28,24 +28,24 @@ struct AppCommand: AsyncParsableCommand {
                                                      password: env.get("DATABASE_PASSWORD") ?? "s3cr3t",
                                                      database: env.get("DATABASE_NAME") ?? "openapi_tutorial",
                                                      tls: .disable))
-    
+
     let api = APIServiceImpl(client: client)
     try api.registerHandlers(on: router)
-    
+
     let app = Application(
       router: router,
       configuration: .init(address: .hostname(hostname, port: port))
     )
-    
+
     try await withThrowingDiscardingTaskGroup { group in
       group.addTask {
         await client.run()
       }
-      
+
       group.addTask {
         try await app.runService()
       }
-      
+
       try await client.query(
                   """
                   CREATE TABLE IF NOT EXISTS openapi_tutorial (
